ab657c8b76b60ab6489cb17321a65f1ff3411b59
sheroanh <anhnguyentuan8@gmail.com>
2023-12-22 14:16:40 +0700

    [22/12] update auth controller

--- Controllers/Auth/AuthController.cs
+++ Controllers/Auth/AuthController.cs
@@ -22,7 +22,7 @@ namespace asp.net.Controllers.Auth
         public string? Password { get; set; }
     }
 
-    public class JWT
+    public class AuthService
     {
         public static string GenerateToken(User user, AuthSetting _authSetting)
         {
@@ -43,6 +43,33 @@ namespace asp.net.Controllers.Auth
                 signingCredentials: credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        public static string ValidateToken(string token, AuthSetting _authSetting)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_authSetting.Jwt.Key);
+            tokenHandler.ValidateToken(token, new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidIssuer = _authSetting.Jwt.Issuer,
+                ValidateAudience = true,
+                ValidAudience = _authSetting.Jwt.Audience,
+                ValidateLifetime = true
+            }, out SecurityToken validatedToken);
+            var jwtToken = (JwtSecurityToken)validatedToken;
+            return jwtToken.Claims.First(x => x.Type == "unique_name").Value;
+        }
+        public static void AddTokenToCookie(HttpResponse response, string token, AuthSetting _authSetting)
+        {
+            response.Cookies.Append("token", token, new CookieOptions
+            {
+                HttpOnly = _authSetting.Cookie.HttpOnly,
+                SameSite = _authSetting.Cookie.SameSite,
+                Expires = DateTime.Now.AddDays(_authSetting.Cookie.MaxAge),
+                Secure = _authSetting.Cookie.Secure
+            });
         }
     }
 
@@ -89,15 +116,9 @@ namespace asp.net.Controllers.Auth
                     code = 400,
                     message = "Tài khoản/mật khẩu không đúng"
                 });
-            var token = JWT.GenerateToken(user, _authSetting);
+            var token = AuthService.GenerateToken(user, _authSetting);
             HttpResponse response = HttpContext.Response;
-            response.Cookies.Append("token", token, new CookieOptions
-            {
-                HttpOnly = _authSetting.Cookie.HttpOnly,
-                SameSite = _authSetting.Cookie.SameSite,
-                Expires = DateTime.Now.AddDays(_authSetting.Cookie.MaxAge),
-                Secure = _authSetting.Cookie.Secure
-            });
+            AuthService.AddTokenToCookie(response, token, _authSetting);
             return Ok(new
             {
                 code = 200,
@@ -114,6 +135,51 @@ namespace asp.net.Controllers.Auth
                         username = user.Username
                     },
                     cart = new List<Array>()
+                }
+            });
+        }
+
+        [HttpPost("dash")]
+        public async Task<ActionResult> DashLogin([FromBody] LoginForm request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var user = await _context.Employees.Where(e => e.User.Username == request.Username).Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                e.User.Username,
+                e.User.Password,
+                e.User.UserRoles
+            }).FirstAsync();
+
+            if (user == null) return BadRequest(new
+            {
+                code = 400,
+                message = "Không tìm thấy tài khoản"
+            });
+
+            if (user != null && !BCrypt.Net.BCrypt.Verify(request.Password, user.Password))
+                return BadRequest(new
+                {
+                    code = 400,
+                    message = "Tài khoản/mật khẩu không đúng"
+                });
+            //var token = AuthService.GenerateToken(user, _authSetting);
+            //HttpResponse response = HttpContext.Response;
+            //AuthService.AddTokenToCookie(response, token, _authSetting);
+            return Ok(new
+            {
+                code = 200,
+                data = new
+                {
+                    //user = new
+                    //{
+                    //    username = user.Username
+                    //}
+                    user
                 }
             });
         }
---
+++ Middlewares/CustomerMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace asp.net.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class CustomerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            if (httpContext.Request.Path == "/v1/customer")
+            {
+
+                return _next(httpContext);
+            }
+            else
+            {
+                return _next(httpContext);
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class CustomerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomerMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomerMiddleware>();
+        }
+    }
+}
