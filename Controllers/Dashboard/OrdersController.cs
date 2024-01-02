b4883d39106d711eda65aeb659512f8f27f52b43
sheroanh <anhnguyentuan8@gmail.com>
2024-01-02 12:46:32 +0700

    [1/1] fix bugs

--- Controllers/Dashboard/OrdersController.cs
+++ Controllers/Dashboard/OrdersController.cs
@@ -781,7 +781,7 @@ namespace asp.net.Controllers.Dashboard
             };
             return Ok(responseSuccess);
         }
-        [HttpGet("statistics/orders")]
+        [HttpGet("statistics")]
         public async Task<ActionResult<IEnumerable<Order>>> GetStatisticsOrders([FromQuery] SearchStatisticDate request)
         {
             if (_context.Orders == null)
